bab45a7144d1e8d08efabb66bbcf15c6913986d9
theanh1508 <85997901+0862551396@users.noreply.github.com>
2022-01-30 23:30:51 +0700

    last time

--- js/handle_Music.js
+++ js/handle_Music.js
@@ -793,15 +793,7 @@ const app = {
                 // }
             }
         }
-        // side_input.addEventListener('keydown',function(event) {   
-        //     if(event.key == "Enter") {
-        //         handle_result() 
-        //         // var windowWidth = $(document).width();
-        //         // if(windowWidth < 1020) {
-        //         //     side_narbar_menu.click()
-        //         // }
-        //     }
-        // })
+        
         
         function handle_result() {
             if(side_input.value != "") {
